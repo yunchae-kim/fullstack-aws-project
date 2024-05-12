743cebdba9787cbd4342ddf8c52ca59c75973103
yunchae-kim <yunchae.dev@gmail.com>
2024-05-11 20:29:10 -0400

    feat: create EC2 instance for file processing
    This commit introduces the creation of an EC2 instance using AWS CDK to
    handle file processing tasks triggered by DynamoDB events.
    
    Key changes:
    
    - Define an `EC2Stack` in `lib/ec2-stack.ts` to create the necessary
      resources for the EC2 instance.
    
    - Create a VPC with a public subnet to host the EC2 instance.
    
    - Set up a security group to control inbound and outbound traffic for
      the EC2 instance.
    
    - Configure the EC2 instance with an appropriate instance type, machine
      image, and security group.
    
    - Update the `bin/app.ts` file to include the `EC2Stack` in the CDK app.
    
    This EC2 instance will be responsible for executing the file processing
    script when triggered by DynamoDB events. The script will download the
    input file from S3, process it, and upload the output file back to S3.

--- infrastructure/bin/app.ts
+++ infrastructure/bin/app.ts
@@ -4,6 +4,7 @@ import { S3Stack } from '../lib/s3-stack';
 import { DynamoDBStack } from '../lib/dynamodb-stack';
 import { LambdaStack } from '../lib/lambda-stack';
 import { ApiGatewayStack } from '../lib/api-gateway-stack';
+import { EC2Stack } from '../lib/ec2-stack';
 
 const app = new App();
 const s3Stack = new S3Stack(app, 'S3Stack');
@@ -15,3 +16,4 @@ const lambdaStack = new LambdaStack(app, 'LambdaStack', {
 new ApiGatewayStack(app, 'ApiGatewayStack', {
   saveToDynamoDBLambda: lambdaStack.saveToDynamoDBLambda,
 });
+new EC2Stack(app, 'EC2Stack');
